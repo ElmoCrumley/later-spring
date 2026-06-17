f7655c384fa61d277b584fa211296ffa500af863
ElmoCrumley <MW826124@gmail.com>
2026-06-17 16:59:01 +0300

    Добавить аннотацию @EnableJpaRepositories(basePackages = "ru.practicum") для сканирования пакетов на содержание репозиториев.
    Добавить "extends JpaRepository<Item, Long>" в класс ItemRepository и "extends JpaRepository<User, Long>" в класс UserRepository.
    
    Классы ItemRepositoryImpl и UserRepositoryImpl больше не нужны.

--- src/main/java/ru/practicum/config/PersistenceConfig.java
+++ src/main/java/ru/practicum/config/PersistenceConfig.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -13,8 +14,10 @@ import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import javax.sql.DataSource;
 import java.util.Properties;
 
+
 @Configuration
 @RequiredArgsConstructor
+@EnableJpaRepositories(basePackages = "ru.practicum")
 public class PersistenceConfig {
     private final Environment environment; // внедряем экземпляр Environment
 
