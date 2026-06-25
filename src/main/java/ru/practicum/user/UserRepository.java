2ca2a67a92c2547d67c8b2e99db71536c26cb65d
ElmoCrumley <MW826124@gmail.com>
2026-06-25 16:01:19 +0300

    Удалить метод save(). Сделать ItemService публичным. Удалить встроенные методы UserRepository. id тоже добавить в UserDTO. Добавить в ItemNote конструктор с параметрами item, text, методы getFormatSaveNoteDate и setFormatSaveNoteDate. Создать ItemNoteController, ItemNoteDto, ItemNoteMapper, ItemNoteService, ItemNoteServiceImpl. В ItemServiceImpl изменить метод addNewItem.

--- src/main/java/ru/practicum/user/UserRepository.java
+++ src/main/java/ru/practicum/user/UserRepository.java
@@ -2,9 +2,5 @@ package ru.practicum.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
-interface UserRepository extends JpaRepository<User, Long> {
-    List<User> findAll();
-    User save(User user);
+public interface UserRepository extends JpaRepository<User, Long> {
 }
